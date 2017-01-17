8678a18faf14cb93ff6395e5b08c6f209c40eff3
RobStek <rob.stekelenburg@idgis.nl>
2017-01-17 13:38:12 +0100

    added more try-outs

--- client/main.js
+++ client/main.js
@@ -3,20 +3,64 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.cmd.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  this.commandInput = new ReactiveVar("> ");
+  this.commandOutput = new ReactiveVar(" - ");
+  
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.cmd.helpers({
+  commandInput() {
+    return Template.instance().commandInput.get();
+  },
+  commandOutput() {
+    return Template.instance().commandOutput.get();
   },
 });
 
-Template.hello.events({
+Template.cmd.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    const commandinput = $('input[name="command"] ');
+    console.log("command = " + commandinput[0].value);
+    instance.commandInput.set("> " + commandinput[0].value);
+    Meteor.call('runCode', commandinput[0].value,  function (err, response) {
+      if (err){
+        console.log("cmd Error = " + err);
+      } else {
+        console.log("cmd Response = " + response);
+        instance.commandOutput.set(response);
+      }
+    });
   },
 });
+
+Template.ziptest.events({
+  'click button'(event, instance) {
+    let zipPath = 'C:/Users/Rob/Downloads/cds-go-live.zip';
+    const zipPathInput = $('input[name="zipPath"] ');
+    if (zipPathInput[0].value){
+      zipPath = zipPathInput[0].value.replace(/\\/gi, '/');
+    }
+    console.log("zipPath = " + zipPath);
+    
+    let outputPath = 'C:/tmp/';
+    const outputPathInput = $('input[name="outputPath"] ');
+    if (outputPathInput[0].value){
+      outputPath = outputPathInput[0].value.replace(/\\/gi, '/');
+    }
+    console.log("outputPath = " + outputPath);
+    
+    Meteor.call('extractZip', zipPath, outputPath,  function (err, response) {
+      if (err){
+        console.log("zip Error = " + err);
+      } else {
+        console.log("zip Response = " + response);
+      }
+    });
+  },
+});
+
+
+ 
--- server/main.js
+++ server/main.js
@@ -1,5 +1,59 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+'use strict';
+
+
+var Future;
+var exec;
+var fs;
+var unzip;
+
+Meteor.startup(function () {
+  // Load future from fibers
+  Future = Npm.require("fibers/future");
+  console.log('Future: ' + Future);
+  // Load exec
+  exec = Npm.require("child_process").exec;
+  console.log('exec: ' + exec);
+  
+  unzip = Npm.require('unzip');
+  console.log('unzip: ' + unzip);
+  
+  fs = Npm.require('fs');
+  console.log('fs: ' + fs);
 });
+
+// Server methods
+Meteor.methods({
+  runCode: function (command) {
+    // This method call won't return immediately, it will wait for the
+    // asynchronous code to finish, so we call unblock to allow this client
+    // to queue other method calls (see Meteor docs)
+    this.unblock();
+    var future=new Future();
+    console.log("command: " + command);
+    console.log('exec: ' + exec);
+    exec(command,function(error,stdout,stderr){
+      if(error){
+        console.log("Error = " + error);
+        future.return(stderr.toString());
+      } else {
+        future.return(stdout.toString());
+      }
+    });
+    return future.wait();
+  }, 
+  
+  extractZip: function (zipPath, outputPath){
+    console.log('fs: ' + fs);
+    console.log('unzip: ' + unzip);
+    try {
+      fs.createReadStream(zipPath).pipe(unzip.Extract({ path: outputPath }));
+      return 'OK';
+    } catch (e){
+      console.log('error: ', e);
+      return e;
+    }
+  }
+});
+ 
